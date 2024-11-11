41ae5499617fcaba084a07f48dd14185c2dd95a0
Raghavendra Naik <raghavendra.naik@kotak.com>
2024-11-12 02:00:30 +0530

    map integration

--- leads_report.py
+++ leads_report.py
@@ -1,3 +1,5 @@
+import folium
+import json
 from leads_geomapper import pair_entities
 from leads_data import read_leads_list_from_excel, read_rm_list_from_excel
 from collections import defaultdict
@@ -80,3 +82,115 @@ print("Below are the availability of all RMs:\n")
 for entry in rm_list:
     print(f"RM Name: {entry['name']} - Availability: {entry['availability_score']}/10")
 print("------------------------------------\n")
+
+
+print("Generate Map with locations\n")
+
+def generate_map_reports(pairs):
+    # Create a map centered at a specific location
+    map_center = [19.107434, 72.890345]  # Coordinates for Mumbai
+    leads_map = folium.Map(location=map_center, zoom_start=12)
+
+    #Darw a connecting line from RM to Lead
+    for pair in pairs:
+        lead = pair['lead']
+        rm = pair['rm']
+        distance = pair['distance']
+        maps_link = pair['maps_link']
+        reason = pair['reason']
+
+        popup_content = f"""
+            <div style="display: flex;align-items: center; flex-direction: column;margin: 5px;align-items: center;">
+                <span>Distance ~ {round(distance)} kms</span>
+                <a href="{maps_link}">Direction <i class="fa-solid fa-diamond-turn-right"></i></a>
+                <span>{reason}</span>
+            </div>
+        """
+        
+        folium.PolyLine(
+            locations=[[rm['lat'], rm['lon']], [lead['lat'], lead['lon']]],
+            color='gray',
+            popup= folium.Popup(popup_content, max_width=250),
+            weight=4,
+            opacity=0.7
+        ).add_to(leads_map)
+
+    all_pins = defaultdict(list)
+    for rm in rm_list:
+        pin = {
+                "name" : rm['name'],
+                "coordinates": [rm['lat'], rm['lon']],
+                "image": f"https://avatar.iran.liara.run/username?username={rm['name']}",
+                "description": [f"Availability: {rm['availability_score']}/10", f"Expertise: {rm['product_expertise']}"]
+            }
+        all_pins["rm"].append(pin)
+
+    for lead in leads_list:
+        pin = {
+                "name" : lead['name'],
+                "coordinates": [lead['lat'], lead['lon']],
+                "image": f"https://avatar.iran.liara.run/username?username={lead['name']}",
+                "description": [f"Propensity: {lead['propensity_score']}", f"Product Pitch: {lead['product_to_pitch']}"]
+            }
+        all_pins["lead"].append(pin)
+
+
+    #print(f"all_pins : {json.dumps(all_pins)}")
+
+    # Add pins to the map with rich content
+    for pin_type, pin_list in all_pins.items():
+        for pin in pin_list:
+
+            popup_description = ""
+            for item in pin['description']:
+                popup_description += f"<span>{item}</span>"
+
+            popup_content = f"""
+            <div style="display:flex">   
+                <div style="display: flex;align-items: center; flex-direction: column;margin: 5px;align-items: center;">
+                    <img src="{pin['image']}" alt="{pin['name']}" style="width: 40px;height:auto;">
+                    <strong>{pin['name']}</strong> 
+                </div>
+                <div style="flex-grow: 1; display:flex; flex-direction:column; align-items:flex-start;"> 
+                    <strong>[{pin_type}]</strong>
+                    {popup_description}
+                </div>
+            </div>
+            """
+            
+            if pin_type == "rm":
+                folium.Marker(
+                    location=pin["coordinates"],
+                    popup=folium.Popup(popup_content, max_width=300),
+                    icon=folium.Icon(color="darkred", prefix="fa", icon="handshake")
+                ).add_to(leads_map)
+            else:
+                folium.Marker(
+                    location=pin["coordinates"],
+                    popup=folium.Popup(popup_content, max_width=300),
+                    icon=folium.Icon(color="green", prefix="fa", icon="user")
+                ).add_to(leads_map)
+    
+        # # Define the coordinates for the polygon (area selection)
+        # polygon_coordinates = [
+        #     [37.7749, -122.4294],  # Top-left corner
+        #     [37.7849, -122.4294],  # Top-right corner
+        #     [37.7849, -122.4094],  # Bottom-right corner
+        #     [37.7749, -122.4094],  # Bottom-left corner
+        # ]
+
+        # # Add the polygon to the map
+        # folium.Polygon(
+        #     locations=polygon_coordinates,
+        #     color='green',
+        #     fill=True,
+        #     fill_color='green',
+        #     fill_opacity=0.5,
+        #     popup='Selected Area'
+        # ).add_to(leads_map)
+
+        # Save the map to an HTML file
+    leads_map.save("report/map_with_area_selection.html")
+    print("Done Map Report")
+
+generate_map_reports(matched_pairs)
